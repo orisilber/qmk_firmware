8e0591925ede47f8d861e424ebfadb8e8c9b1584
ori silber <orizilber97@gmail.com>
2021-08-30 01:23:51 +0300

    feature: add tap hold to center space

--- keyboards/mechwild/mercutio/keymaps/bongocat/keymap.c
+++ keyboards/mechwild/mercutio/keymaps/bongocat/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       MO(1),            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
       KC_LSFT, KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_RSFT,
-      KC_LCTL, KC_LALT, KC_LGUI,          TD(),  LT(2, KC_SPC),    KC_SPC,           KC_RALT, KC_LGUI,          KC_RCTL ),
+      KC_LCTL, KC_LALT, KC_LGUI,          KC_SPC,  LT(2, KC_SPC),    KC_SPC,           KC_RALT, KC_LGUI,          KC_RCTL ),
 
   [1] = LAYOUT_all(
                                                                                                                            KC_TRNS,
